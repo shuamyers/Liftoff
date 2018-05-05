2eccadbdc1da6fdff3d80f696dc153aa0a12e610
Shua Myers <sym300@gmail.com>
2018-05-06 00:16:28 +0300

    moment filter

--- frontend/src/vueFilters.js
+++ frontend/src/vueFilters.js
@@ -8,7 +8,6 @@ Vue.filter('daysRemaining', function(date) {
         var endDate = moment('1525561200','X');
         var now = moment()
         var diff = endDate.diff(now, 'days')
-        console.log(diff)
         if(diff === 0) str = (diff + 1) + ' day left'
         if(diff === 1) str = (diff) + ' day left'
         else if (diff > 1)  str = diff + ' days left'
