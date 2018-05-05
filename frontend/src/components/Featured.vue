1e7e15227f62d85d2713014a7a882c975f81082c
Ophir Yael <ofir.yael@gmail.com>
2018-05-05 17:51:44 +0300

    first homepage carousel setup

--- frontend/src/components/Featured.vue
+++ frontend/src/components/Featured.vue
@@ -2,12 +2,26 @@
   <v-carousel
     v-if="featuredProjs" 
     :hide-controls="false"
-    :hide-delimiters="false">
+    :hide-delimiters="true">
 
     <v-carousel-item 
       v-for="(proj, i) in featuredProjs" 
       :src="proj.featuredImgUrl" 
       :key="i">
+
+      <v-jumbotron dark :to="`/project/${proj._id}`">
+        <v-container fill-height>
+          <v-layout class="align-end">
+            <v-flex style="max-width: 350px" >
+              <h4 class="featured-txt body-1 mb-3">Featured</h4>
+              <h3 class="title mb-2">{{ proj.title }}</h3>
+              <p class="body-1">{{ proj.desc }}</p>
+              <p class="campaign-link body-2">See Campaign &gt;</p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
+
     </v-carousel-item>
 
   </v-carousel>
@@ -27,6 +41,28 @@ export default {
     projService.query({ category: "Tech" }).then(projs => {
       this.featuredProjs = projs;
     });
-  }
+  },
+  updated() {
+      const imgs = document.querySelectorAll('.jumbotron__image');
+      imgs.forEach(img => {
+        img.setAttribute('style', 'filter: brightness(50%)')
+        });
+    }
 };
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+
+.featured-txt {
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  border-bottom: 1px solid #fff;
+}
+
+.campaign-link {
+  text-transform: uppercase;  
+}
+</style>
