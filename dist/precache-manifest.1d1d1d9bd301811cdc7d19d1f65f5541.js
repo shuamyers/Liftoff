f7d1a305b063bfe250f7bd47ed1bfc19d3ed7491
Shua Myers <sym300@gmail.com>
2018-05-10 17:33:31 +0300

    deleted dist .gitignore

---
+++ dist/precache-manifest.1d1d1d9bd301811cdc7d19d1f65f5541.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fddc98095c27699cc55",
+    "url": "/js/vendor.1fddc980.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "e79d4b11dee547f330f6",
+    "url": "/js/app.e79d4b11.js"
+  },
+  {
+    "revision": "3f372708daf627633ba9b3677be53c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38df46234088dfdb59ded7f2575c8cd4",
+    "url": "/img/logo1.png"
+  },
+  {
+    "revision": "a5501e1a244397b1038bbdc6e700aa30",
+    "url": "/img/logo1 (2).png"
+  },
+  {
+    "revision": "68652cd11ca97a33a60a8665542bd40d",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/favicon1.ico"
+  },
+  {
+    "revision": "e79d4b11dee547f330f6",
+    "url": "/css/app.0a015d6c.css"
+  }
+];
---
+++ dist/precache-manifest.76faa0877a2ed4d79527309ae33f70ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fddc98095c27699cc55",
+    "url": "/js/vendor.1fddc980.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "e0175952360ac9559f41",
+    "url": "/js/app.e0175952.js"
+  },
+  {
+    "revision": "feedb9ac8cf91c7b23d114f7c48e3b8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38df46234088dfdb59ded7f2575c8cd4",
+    "url": "/img/logo1.png"
+  },
+  {
+    "revision": "a5501e1a244397b1038bbdc6e700aa30",
+    "url": "/img/logo1 (2).png"
+  },
+  {
+    "revision": "68652cd11ca97a33a60a8665542bd40d",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/favicon1.ico"
+  },
+  {
+    "revision": "e0175952360ac9559f41",
+    "url": "/css/app.0a015d6c.css"
+  }
+];
