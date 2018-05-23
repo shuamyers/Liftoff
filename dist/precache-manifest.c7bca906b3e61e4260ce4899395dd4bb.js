f3b8d9d259a879c574f8dd4ecef22c69bc768aaf
Ophir Yael <ofir.yael@gmail.com>
2018-05-23 19:33:00 +0300

    updated dist

---
+++ dist/precache-manifest.c7bca906b3e61e4260ce4899395dd4bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e0a60f251bdab471132",
+    "url": "/js/vendor.0e0a60f2.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "4fa742f0cc512279b44c",
+    "url": "/js/app.4fa742f0.js"
+  },
+  {
+    "revision": "9324306e03c925a42cf6f5b03805bf42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38df46234088dfdb59ded7f2575c8cd4",
+    "url": "/img/logo1.png"
+  },
+  {
+    "revision": "a5501e1a244397b1038bbdc6e700aa30",
+    "url": "/img/logo1 (2).png"
+  },
+  {
+    "revision": "68652cd11ca97a33a60a8665542bd40d",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/favicon1.ico"
+  },
+  {
+    "revision": "4fa742f0cc512279b44c",
+    "url": "/css/app.883bd416.css"
+  }
+];
